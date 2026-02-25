ba990ff6f6cb12c9168b2e117c1107530e715e55
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-25 00:33:03 -0500

    Update load_test.sh

--- load_test.sh
+++ load_test.sh
@@ -2,4 +2,3 @@ sudo apt update
 sudo apt install nodejs npm
 sudo npm install -g autocannon
 autocannon -v
-autocannon -p 5 -a 50 -m POST -H "Content-Type: application/json" -b '{{ data }}' {{ endpoint }}
